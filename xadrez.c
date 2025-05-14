e5d9329d64c0cd52a6f67175b2728b9f28308e52
202503013462 <202503013462@alunos.estacio.br>
2025-05-14 12:36:00 -0300

    Create xadrez.c

---
+++ xadrez.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+
+    /*
+      Este programa faz a leitura de informacoes de cidades e as exibe no final das entradas, serao inseridas informacoes de duas cidades
+    */
+
+    // declaracao de variaveis para dois armazenamentos simples
+
+    // primeira peça
+
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+  return 0;
+}
