c68b9414b2e05b395d74ee2cbe663c3eeda58f96
202503013462 <202503013462@alunos.estacio.br>
2025-05-14 12:43:55 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -3,16 +3,35 @@
 int main() {
 
     /*
-      Este programa faz a leitura de informacoes de cidades e as exibe no final das entradas, serao inseridas informacoes de duas cidades
+      Este programa faz o movimento de tres peças: 
+      - Torre: Move-se em linha reta horizontalmente ou verticalmente. Seu programa deverá simular o movimento da Torre cinco casas para a direita.
+      - Bispo: Move-se na diagonal. Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita. Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
+      - Rainha: Move-se em todas as direções. Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
     */
 
-    // declaracao de variaveis para dois armazenamentos simples
+    // primeira peça: Torre
 
-    // primeira peça
-
+    printf("Movimento da Primeira Peça = Torre - 5 casas para Direita\n");
+ 
     for (int i = 0; i < 5; i++) {
         printf("Direita\n");
     }
 
+    // segunda peça: Bispo
+
+    printf("Movimento da Primeira Peça = Bispo - 5 casas na diagonal para cima e a direita\n");
+ 
+    for (int i = 0; i < 5; i++) {
+        printf("Cima, Direita\n");
+    }
+
+    // terceira peça: Rainha
+
+    printf("Movimento da Primeira Peça = Torre - 8 casas para esquerda\n");
+ 
+    for (int i = 0; i < 8; i++) {
+        printf("Esquerda\n");
+    }
+ 
   return 0;
 }
